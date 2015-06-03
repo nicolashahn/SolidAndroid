f0d364e69cfc15733766012579fa4eddcdaf9935
Nicolas Hahn <nicolas@stonespring.org>
2015-06-02 18:32:35 -0700

    UserModel class added with 5 fields

---
+++ uIAppTemplate/src/main/java/com/csform/android/uiapptemplate/model/UserModel.java
@@ -0,0 +1,80 @@
+package com.csform.android.uiapptemplate.model;
+
+import java.io.Serializable;
+
+/**
+ * Created by Nick on 6/2/2015.
+ */
+public class UserModel implements Serializable {
+    private String dateJoined;
+    private String email;
+    private String name;
+    private String phone;
+//    private Integer rating;
+    private String avatar;
+
+    public UserModel(){
+        dateJoined = "";
+        email = "";
+        name = "";
+        phone = "";
+        avatar = "";
+    }
+
+    public UserModel(String dj, String e, String n, String p, String a){
+        dateJoined = dj;
+        email = e;
+        name = n;
+        phone = p;
+        avatar = a;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String e) {
+        email = e;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String a) {
+        avatar = a;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String n) {
+        name = n;
+    }
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String p) {
+        phone = p;
+    }
+
+    public String getDateJoined() {
+        return dateJoined;
+    }
+
+    public void setDateJoined(String dj) {
+        dateJoined = dj;
+    }
+
+    @Override
+    public String toString() {
+        return email;
+    }
+
+    // no purpose other than to satisfy the abstract method Adapter
+    public long getId() {
+        return 0;
+    }
+}
