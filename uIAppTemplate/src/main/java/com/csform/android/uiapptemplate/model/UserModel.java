01f794608ca88c1e82d2762721f6e340981cabaa
Nicolas Hahn <nicolas@stonespring.org>
2015-06-06 20:07:22 -0700

    userModel: storeInfo(), getInfo() added

--- uIAppTemplate/src/main/java/com/csform/android/uiapptemplate/model/UserModel.java
+++ uIAppTemplate/src/main/java/com/csform/android/uiapptemplate/model/UserModel.java
@@ -1,5 +1,8 @@
 package com.csform.android.uiapptemplate.model;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
 import java.io.Serializable;
@@ -8,6 +11,8 @@ import java.io.Serializable;
  * Created by Nick on 6/2/2015.
  */
 public class UserModel implements Serializable {
+    private String LOG_TAG = "UserModel";
+
     private String dateJoined;
     private String email;
     private String name;
@@ -15,20 +20,62 @@ public class UserModel implements Serializable {
 //    private Integer rating;
     private String avatar;
 
+    private static UserModel instance = null;
+
+    // given no arguments, creates a default UserModel instance
     public UserModel(){
-        dateJoined = "";
-        email = "";
-        name = "";
-        phone = "";
-        avatar = "";
+        dateJoined = "defaultDate";
+        email = "default@email.com";
+        name = "Default Name";
+        phone = "5551234567";
+        avatar = "no avatar";
     }
 
-    public UserModel(String dj, String e, String n, String p, String a){
+
+    public UserModel( String dj, String e, String n, String p, String a){
         dateJoined = dj;
         email = e;
         name = n;
         phone = p;
         avatar = a;
+    }
+
+    // given context and all fields of UserModel, store in SharedPreferences
+    public static void storeInfo(Context context, String dj, String e, String n, String p, String a){
+        SharedPreferences settings =
+                PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString("dateJoined", dj);
+        editor.putString("email",e);
+        editor.putString("name",n);
+        editor.putString("phone",p);
+        editor.putString("avatar",a);
+        editor.commit();
+    }
+
+    public static UserModel getInfo(Context context){
+        SharedPreferences settings =
+                PreferenceManager.getDefaultSharedPreferences(context);
+        String dj = "defValue";
+        String n = "defValue";
+        String e = "defValue";
+        String p = "defValue";
+        String a = "defValue";
+        // Try getting the name first - if we can, then get the rest of the strings
+        // relies on the assumption that if n is not null, the other fields will also
+        // not be null.
+        settings.getString("name", n);
+        if (n != null && n != "defValue"){
+            settings.getString("dateJoined",dj);
+            settings.getString("email",e);
+            settings.getString("phone",p);
+            settings.getString("avatar",a);
+            UserModel um = new UserModel(dj, e, n, p, a);
+            return um;
+        }
+
+        // if we can't get the data from SharedPreferences, just return a default user
+        return new UserModel();
     }
 
     public String getEmail() {
@@ -48,12 +95,12 @@ public class UserModel implements Serializable {
     }
 
     public String getName() {
-        Log.i("returninig", name);
+        Log.i("returning name "+name, name);
         return name;
     }
 
     public void setName(String n) {
-        Log.i("setting, ", n);
+        Log.i("setting name "+n, n);
         name = n;
     }
     public String getPhone() {
