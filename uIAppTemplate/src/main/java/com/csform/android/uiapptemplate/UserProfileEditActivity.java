63d350b7f275da50b61b16b74d39c517a174ccfe
Ashley Rae Rocha <ash0arr@gmail.com>
2015-06-07 20:15:33 -0700

    you can edit your name and phone number

--- uIAppTemplate/src/main/java/com/csform/android/uiapptemplate/UserProfileEditActivity.java
+++ uIAppTemplate/src/main/java/com/csform/android/uiapptemplate/UserProfileEditActivity.java
@@ -1,13 +1,27 @@
 package com.csform.android.uiapptemplate;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.csform.android.uiapptemplate.model.UserModel;
+import com.csform.android.uiapptemplate.util.ImageUtil;
+import com.firebase.client.Firebase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class UserProfileEditActivity extends ActionBarActivity {
+    private EditText nameField;
+    private EditText phoneField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,18 +29,64 @@ public class UserProfileEditActivity extends ActionBarActivity {
         setContentView(R.layout.activity_user_profile_edit);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        TextView title = (TextView) findViewById(R.id.titleText);
-        TextView desc = (TextView) findViewById(R.id.descText);
-        TextView user = (TextView) findViewById(R.id.userText);
-        TextView datePosted = (TextView) findViewById(R.id.postedDateText);
-        TextView dateDoneBy = (TextView) findViewById(R.id.completedDateText);
-        TextView compensation = (TextView) findViewById(R.id.compText);
+        nameField = (EditText) findViewById(R.id.nameEdit);
+        nameField.setText(UserModel.getField(this, "name"));
+        phoneField = (EditText) findViewById(R.id.phoneEdit);
+        phoneField.setText(UserModel.getField(this, "phone"));
+        TextView emailEdit = (TextView) findViewById(R.id.emailView);
+        emailEdit.setText(UserModel.getField(this, "email"));
+        ImageView avatarView = (ImageView) findViewById(R.id.image);
+        ImageUtil.displayImage(avatarView, UserModel.getField(this, "avatar"), null);
+        TextView cancelButton = (TextView) findViewById(R.id.cancel);
+        cancelButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        TextView saveButton = (TextView) findViewById(R.id.saveChanges);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                saveChanges();
+            }
+        });
     }
 
+    private void saveChanges() {
+        final String nameEdit = nameField.getText().toString();
+        final String phoneEdit = phoneField.getText().toString();
+
+        if (nameEdit.matches("") || phoneEdit.matches("")) {
+            Context context = getApplicationContext();
+            CharSequence text = "You cannot leave a field blank";
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+            return;
+        }
+        if (phoneEdit.length() != 10) {
+            Context context = getApplicationContext();
+            CharSequence text = "Phone number must be 10 digits";
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+            return;
+        }
+        final Firebase ref = new Firebase("https://crackling-torch-5178.firebaseio.com");
+        final Firebase userRef = ref.child("users").child(emailToKey(UserModel.getField(this, "email")));
+        Map<String, Object> userData = new HashMap<>();
+        userData.put("name", nameEdit);
+        userData.put("phone", phoneEdit);
+        userRef.updateChildren(userData);
+                /*
+                 * How do I update User Model here?
+                 * UserModel.setField(this, "phone", "newPhone");
+                 */
+        finish();
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_favor_spec, menu);
+//        getMenuInflater().inflate(R.menu.menu_favor_spec, menu);
         return true;
     }
 
@@ -38,4 +98,8 @@ public class UserProfileEditActivity extends ActionBarActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+	public String emailToKey(String emailAddress) {
+		return emailAddress.replace('.', ',');
+	}
 }
